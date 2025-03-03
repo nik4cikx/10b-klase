0c19ed996dbd4b59108dbcc0dd4d2cc8493abd15
gunca16 <gunca1316@gmail.com>
2025-03-03 09:22:54 +0200

    03.03.2025

---
+++ 2.semestris programmēšana/17.02.2025/24.02.2025/script.js
@@ -0,0 +1,49 @@
+/*
+let a = 5;
+let b = "5";
+ 
+
+if( a == b)
+{
+    console.log("a == b");
+}
+if( a === b)
+{
+    console.log("a === b");
+}
+*/
+
+let diena =parseInt(prompt("Ievadi dienas nummuru"));
+
+switch( diena )
+{
+    case 1:
+        console.log("Pirmdiena");
+        
+        break;
+
+        case 2:
+        console.log("Otrdiena");
+        break;
+
+        case 3:
+            console.log("Trešdiena");
+            break;
+
+            case 4:
+                console.log("Ceturtdiena");
+                break;
+
+    case 5:
+        console.log("Piektdiena");
+        break;
+
+        case 6:
+            console.log("Sestdiena");
+            break;
+
+            case 7:
+        console.log("Svētdiena");
+        break;
+
+    }
